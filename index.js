f385c8fd6c1f6cde23969465d5568c366829ffa7
Elnathan Mburu <elnathanmburu@gmail.com>
2023-10-21 21:20:27 +0300

    done

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 function isPalindrome(word) {
+  var check= Math.floor(word.length/2)
+  for (i=0;i<check;i++){
+    if (word[i] !==word[word.length-1])
+    return false;
+  else return true;
+  }
+};
+console.log(isPalindrome("mom"));
+
   // Write your algorithm here
-}
+ /* input word the word 
+ if the word is palindrome return true 
+ if the word is not a palindrome return false
+
+
 
 /* 
   Add your pseudocode here
+  function isPalindrone 
+  reverseStr =reverse(str)
+   if str is equal to reversedStr
+        return True
+    else
+        return False
+    end if
+end function
+  
 */
 
 /*
   Add written explanation of your solution here
+  palindrone letters are the same from beginning to end except for one in between letter
+  so the first letter should be equaal to the last letter
 */
-
+/*boow");isPalindrome("
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
@@ -22,4 +46,5 @@ if (require.main === module) {
   console.log("=>", isPalindrome("robot"));
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome;*/
+
